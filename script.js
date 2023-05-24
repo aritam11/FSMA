250a27ab56498e6bc243f29c6e34408e97e0f97c
Aritam <aritamasthana@gmail.com>
2023-05-25 00:27:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculatePrice() {
+    var product_price_inr = parseFloat(document.getElementById("product_price_inr").value);
+    var usd_to_inr_rate = parseFloat(document.getElementById("usd_to_inr_rate").value);
+    var current_usd_to_inr_rate = parseFloat(document.getElementById("current_usd_to_inr_rate").value);
+
+    var adjusted_price_inr = product_price_inr * (current_usd_to_inr_rate / usd_to_inr_rate);
+
+    document.getElementById("adjusted_price_inr").textContent = "Current FSMA price (INR) subjected to current USD to INR rate: " + adjusted_price_inr.toFixed(2);
+}
