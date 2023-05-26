f609df91eea7880234df0ee44e5b5f1718c5bf21
Aritam <aritamasthana@gmail.com>
2023-05-26 16:57:25 +0530

    rounding off error

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ function calculatePrice() {
     var product_price_inr = parseFloat(document.getElementById("product_price_inr").value.substring(1));
     var usd_to_inr_rate = parseFloat(document.getElementById("usd_to_inr_rate").value.substring(1));
     var current_usd_to_inr_rate = parseFloat(document.getElementById("current_usd_to_inr_rate").value.substring(1));
-    console.log((current_usd_to_inr_rate-usd_to_inr_rate).toFixed(2))
+    console.log(((((current_usd_to_inr_rate) - usd_to_inr_rate).toFixed(2)/ usd_to_inr_rate)*100) <= -0.5)
     console.log((((current_usd_to_inr_rate) - usd_to_inr_rate).toFixed(2)/ usd_to_inr_rate)*100)
     if(((((current_usd_to_inr_rate) - usd_to_inr_rate).toFixed(2)/ usd_to_inr_rate)*100) >= 0.5 || ((((current_usd_to_inr_rate) - usd_to_inr_rate).toFixed(2)/ usd_to_inr_rate)*100) <= -0.5)
     {
-        var adjusted_price_inr = product_price_inr + product_price_inr * (Math.round((((current_usd_to_inr_rate) - usd_to_inr_rate).toFixed(2)/ usd_to_inr_rate)*100))/100;
+        if ((((current_usd_to_inr_rate) - usd_to_inr_rate).toFixed(2)/ usd_to_inr_rate)*100 % 1 === -0.5) {
+            var adjusted_price_inr = product_price_inr + product_price_inr * Math.floor((((current_usd_to_inr_rate) - usd_to_inr_rate).toFixed(2)/ usd_to_inr_rate)*100)/100;}
+        else{
+            var adjusted_price_inr = product_price_inr + product_price_inr * (Math.round((((current_usd_to_inr_rate) - usd_to_inr_rate).toFixed(2)/ usd_to_inr_rate)*100))/100;
+        }
+            
     }
     else{
         var adjusted_price_inr = product_price_inr;
